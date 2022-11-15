24d786c1f8ac856f318cfb5dd337131e9994aa7f
zzy <1246486788@qq.com>
2022-11-15 13:27:24 +0800

    初始化界面和文件

---
+++ js/tab.js
@@ -0,0 +1,28 @@
+class Tab {
+    constructor(id) {
+        //获取元素
+        this.main = document.querySelector(id);
+    }
+
+    //1.切换
+    toggleTab() {
+
+    }
+
+    //2.添加
+    addTab() {
+
+    }
+
+    //3.删除
+    removeTab() {
+
+    }
+
+    //4.修改
+    editTab() {
+
+    }
+}
+
+new Tab('#tab');
